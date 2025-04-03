919db375caaf88d14b231d410fe3ec0d6bafab30
Giovanni <giovanni.10leao@hotmail.com>
2025-04-02 21:47:39 -0300

    1002

--- 1001 - URI.js
+++ 1001 - URI.js
@@ -1,6 +1,6 @@
 var input = require("fs").readFileSync("stdin", "utf8");
 
-const [a, b] = InputDeviceInfo.split('\n').map(Number);
+const [a, b] = input.split('\n').map(Number);
 x = a+b;
 
 console.log(`X = ${x}`);
