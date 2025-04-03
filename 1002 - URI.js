e7d3c8432f19e32b04afba2a2364ab86df122f44
Giovanni <giovanni.10leao@hotmail.com>
2025-04-02 21:51:13 -0300

    !

--- 1002 - URI.js
+++ 1002 - URI.js
@@ -1,4 +1,4 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var input = require('fs').readFileSync('stdin', 'utf8');
 var [r] = input.split('\n').map(Number);
 var x = (r*r)*(3.14159);
 console.log(`A=${x.toFixed(4)}`);
